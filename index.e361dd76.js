59833d386920a752646e2da0a3b322c403b6df5a
Vasyl24 <118126367+Vasyl24@users.noreply.github.com>
2024-08-03 21:15:17 +0000

    Deploying to gh-pages from @ Vasyl24/lazar-development@41fea19038f08d6b6695414c2b9933c136d3c24f 🚀

---
+++ index.e361dd76.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-menu-container"),t=document.querySelector(".js-open-menu");t.addEventListener("click",(function(){var o="true"===t.getAttribute("aria-expanded")||!1;t.setAttribute("aria-expanded",!o),e.classList.toggle("is-open"),bodyScrollLock[o?"enableBodyScroll":"disableBodyScroll"](document.body)}))}();
+//# sourceMappingURL=index.e361dd76.js.map
